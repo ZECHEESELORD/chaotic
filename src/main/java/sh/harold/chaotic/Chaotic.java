c0e9a94e1ff46dd9181aed566afea73188a60c9e
🧀 Harold <55301634+ZECHEESELORD@users.noreply.github.com>
2025-11-20 02:56:12 -0500

    init commit

---
+++ src/main/java/sh/harold/chaotic/Chaotic.java
@@ -0,0 +1,17 @@
+package sh.harold.chaotic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Chaotic extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
